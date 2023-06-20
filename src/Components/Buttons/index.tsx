95b43b2089ba8887a1a4dcf66515dcb351a5b688
carlos cardoso <carloscardosojk@gmail.com>
2023-06-20 20:52:35 -0300

    update

--- src/Components/Buttons/index.tsx
+++ src/Components/Buttons/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-interface ButtonPrps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   onClick: () => void;
   isDisabled: boolean;
   text: string;
 }
 
-export const Button = (props: ButtonPrps) => {
+export const Button = (props: ButtonProps) => {
   return (
     <button {...props} onClick={props.onClick} disabled={props.isDisabled}>
       {props.text}
